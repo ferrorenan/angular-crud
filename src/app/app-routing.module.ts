2da3d2921c2555a83def85a2d19b8de28d3976d0
Renan Ferro <renan.ferro@fmd.ag>
2022-06-14 22:00:00 -0300

    feat(rota): adiciona rota para dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/dashboard/dashboard.module')
+        .then(module => module.DashboardModule),
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
