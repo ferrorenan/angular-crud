8c5486b2e45ceabd260f4b92d7cbe4585040e55c
Renan Ferro <renan.ferro@fmd.ag>
2022-06-16 13:13:52 -0300

    feat(user model): create new user model

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from '../../services/user-data.service';
+import { catchError, delay, Observable, Subject } from 'rxjs';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  userData$: Observable<User> | undefined;
+  userDataApiError$ = new Subject<boolean>();
+
+  constructor(
+      private getUserDataService: UserDataService
+  ) { }
 
   ngOnInit(): void {
+    this.getUserData();
   }
 
+  getUserData(): void {
+
+    this.userData$ =
+        this.getUserDataService
+            .getUserData()
+            .pipe(
+                delay(5000),
+                catchError((error) => {
+                  this.userDataApiError$.next(true);
+                  throw error;
+                })
+            );
+  }
 }
