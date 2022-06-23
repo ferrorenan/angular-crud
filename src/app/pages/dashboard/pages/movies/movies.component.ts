7f32a813ef4a2cff920bb14eda41c7a11be706be
Renan Ferro <renan.ferro@fmd.ag>
2022-06-23 16:41:53 -0300

    feat(movies): imeplementa tela para filmes

--- src/app/pages/dashboard/pages/movies/movies.component.ts
+++ src/app/pages/dashboard/pages/movies/movies.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../../../services/movies.service';
+import { Movies } from '../../../../models/movies';
+import { Observable } from 'rxjs';
+import { animateListItems } from '../../../../animations/animations';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
+  animations: [
+      animateListItems
+  ]
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  movies$: Observable<Movies[]> | undefined;
+
+  constructor(
+      private _moviesService: MoviesService
+  ) { }
 
   ngOnInit(): void {
+    this.getMovies();
   }
 
+  getMovies(): void {
+
+    this.movies$ = this._moviesService.getMovies();
+  }
 }
--- src/app/pages/dashboard/pages/movies/movies.module.ts
+++ src/app/pages/dashboard/pages/movies/movies.module.ts
@@ -3,6 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
+import {
+  PanelContentModule
+} from '../../../../components/panel-content/panel-content.module';
+import {
+  StructureContainerModule
+} from '../../../../components/structure-container/structure-container.module';
+import {
+  ButtonBackModule
+} from '../../../../components/button-back/button-back.module';
+import { CardModule } from '../../components/card/card.module';
+import {
+  CardPanelModule
+} from '../../../../components/card-panel/card-panel.module';
 
 
 @NgModule({
@@ -11,7 +24,12 @@ import { MoviesComponent } from './movies.component';
   ],
   imports: [
     CommonModule,
-    MoviesRoutingModule
-  ]
+    MoviesRoutingModule,
+    PanelContentModule,
+    StructureContainerModule,
+    ButtonBackModule,
+    CardModule,
+    CardPanelModule,
+  ],
 })
 export class MoviesModule { }
