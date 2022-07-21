4fd17876c36e7e848a73aeebf4ead1cd2d80ecf8
Renan Ferro <renan.ferro@fmd.ag>
2022-07-21 20:36:04 -0300

    feat(estrutura): ajusta estruutra de componente panel

--- src/app/components/panel-content/panel-content.component.ts
+++ src/app/components/panel-content/panel-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-panel-content',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel-content.component.scss']
 })
 export class PanelContentComponent implements OnInit {
+
+  @Input() additionalClass = '';
 
   constructor() { }
 
