bcf59c6bccd6ac86707d28575eacabf0df2fb644
Renan Ferro <renan.ferro@fmd.ag>
2022-08-10 21:13:36 -0300

    feat(pwa): instala pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoadingModule } from './components/loading/loading.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,12 @@ import { LoadingModule } from './components/loading/loading.module';
     HttpClientModule,
     BrowserAnimationsModule,
     LoadingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
