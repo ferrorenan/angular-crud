f63faed7cb1aaed5808cf9c7f512ff14a771ac48
Renan Ferro <renan.ferro@fmd.ag>
2022-08-16 21:28:49 -0300

    feat(movies form): adiciona nova folha para form de filmes

--- src/app/pages/auth-area/movies-form/movies-form-routing.module.ts
+++ src/app/pages/auth-area/movies-form/movies-form-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MoviesComponent } from './movies.component';
+import { MoviesFormComponent } from './movies-form.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MoviesComponent,
-    title: 'Movies'
+    component: MoviesFormComponent
   }
 ];
 
@@ -14,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MoviesRoutingModule { }
+export class MoviesFormRoutingModule { }
---
+++ src/app/pages/auth-area/movies-form/movies-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-form',
+  templateUrl: './movies-form.component.html',
+  styleUrls: ['./movies-form.component.scss']
+})
+export class MoviesFormComponent implements  OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getFormValue(formValue: any): void {
+
+    console.log(formValue);
+  }
+}
