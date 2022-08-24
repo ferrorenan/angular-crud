9975fe96fed28e34488e40e55a45ff2f69c97d0f
Renan Ferro <renan.ferro@fmd.ag>
2022-08-24 15:10:58 -0300

    feat(header): adiciona componente para toggle dark/light mode

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { LinkModule } from '../link/link.module';
+import {
+  ToggleThemeSiteModule
+} from '../toggle-theme-site/toggle-theme-site.module';
 
 
 
@@ -12,6 +15,7 @@ import { LinkModule } from '../link/link.module';
   imports: [
     CommonModule,
     LinkModule,
+    ToggleThemeSiteModule,
   ],
   exports: [
     HeaderComponent
