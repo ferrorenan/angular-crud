b57e9f214fe93144cadc19a4d682fdb87d3c5481
Renan Ferro <renan.ferro@fmd.ag>
2023-02-08 18:13:15 -0300

    feat(form): insert new form structure

--- src/app/components/form-components/form/form.component.ts
+++ src/app/components/form-components/form/form.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BaseFormComponent } from '../base-form/base-form.component';
 import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AlertService } from '../../../services/alert.service';
+import { MoviesService } from '../../../services/movies.service';
 
 @Component({
   selector: 'app-form',
@@ -9,10 +12,16 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class FormComponent extends BaseFormComponent implements OnInit {
 
+  passWord = 'renanferro';
+  formData = '';
+
   @Output() formValueOutput = new EventEmitter<any>();
+  @Input() typeOfManager: string | null = '';
 
   constructor(
       private _formBuilder: FormBuilder,
+      private _alertService: AlertService,
+      private _moviesService: MoviesService,
   ) {
     super();
   }
@@ -20,6 +29,7 @@ export class FormComponent extends BaseFormComponent implements OnInit {
   ngOnInit(): void {
 
     this.buildForm();
+
   }
 
   buildForm(): void {
@@ -34,11 +44,70 @@ export class FormComponent extends BaseFormComponent implements OnInit {
 
     !this.formulary.valid
         ? this.verifyFormValidations(this.formulary)
-        : this.emmitFormValue();
+        : this.submitFormValue();
   }
 
-  emmitFormValue(): void {
+  submitFormValue(): void {
 
-    this.formValueOutput.emit(this.formulary.getRawValue());
+
+    Swal.fire({
+      title: 'Insert the password to add',
+      input: 'password',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Validate',
+      showLoaderOnConfirm: true,
+      preConfirm: (login) => {
+        if (login != this.passWord) {
+
+          this._alertService
+              .alertWithConfirmation(
+                  'Ooopss!',
+                  'Something is wrong!',
+                  'error',
+              );
+        } else {
+
+          this.insertNewProductToMarketList();
+        }
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    })
+  }
+
+  insertNewProductToMarketList(): void {
+
+    this._alertService
+        .alertWithConfirmation(
+            'Nice!',
+            'Please wait, we are adding the new item!',
+            'info',
+        );
+
+    this._moviesService.insertMovies(this.formulary.getRawValue())
+        .subscribe({
+          next: ((response) => {
+            console.log(response);
+          }),
+          error: ((error) => {
+            console.log(error);
+            this._alertService
+                .showFeedbackClient(
+                    'OooOuuhH!',
+                    'Something happened and we had a error!',
+                    'error',
+                );
+          }),
+          complete: (() => {
+            this._alertService
+                .showFeedbackClient(
+                    'New item added!',
+                    'Your new item was added in your market list!',
+                    'success',
+                );
+          })
+        })
   }
 }
