e634b1877781c646c862949a64e653b26a3c515e
Renan Ferro <renan.ferro@fmd.ag>
2023-02-13 21:26:21 -0300

    feat(animations): insert new animation structure

--- src/app/animations/route-animations.ts
+++ src/app/animations/route-animations.ts
@@ -1,5 +1,5 @@
 import {
-  animate,
+  animate, group,
   query,
   style,
   transition,
@@ -8,24 +8,64 @@ import {
 
 export const fader =
     trigger('routerAnimations', [
-        transition('* <=> *', [
-            query(':enter, :leave', [
-                style({
+      transition('Home <=> About', [
+        query(':enter, :leave', [
+          style({
 
-                  position: 'absolute',
-                  left: 0,
-                  width: '100%',
-                  opacity: .7,
-                  transform: 'translateX(100%)'
-                })
-            ]),
-            query(':enter', [
-                animate('1s ease',
-                      style({
-                        opacity: 1,
-                        transform: 'translateX(0)'
-                      })
-                    )
-            ])
+            position: 'absolute',
+            left: 0,
+            width: '100%',
+            opacity: .7,
+            transform: 'translateY(100%)'
+          })
+        ]),
+        query(':enter', [
+          animate('1s ease',
+              style({
+                opacity: 1,
+                transform: 'translateY(0)'
+              })
+          )
         ])
+      ]),
+      transition('* => MarketList', [
+        query(':enter, :leave', [
+          style({
+
+            position: 'absolute',
+            left: 0,
+            width: '100%',
+            opacity: .7,
+            transform: 'translateY(-100%)'
+          })
+        ]),
+        query(':enter', [
+          animate('1s ease',
+              style({
+                opacity: 1,
+                transform: 'translateY(0)'
+              })
+          )
+        ])
+      ]),
+      transition('MarketList => *', [
+        query(':enter, :leave', [
+          style({
+
+            position: 'absolute',
+            left: 0,
+            width: '100%',
+            opacity: .7,
+            transform: 'translateX(-100%)'
+          })
+        ]),
+        query(':enter', [
+          animate('1s ease',
+              style({
+                opacity: 1,
+                transform: 'translateY(0)'
+              })
+          )
+        ])
+      ])
     ])
