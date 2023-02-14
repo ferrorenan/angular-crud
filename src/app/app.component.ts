c4f63d9bf29e7ce4e25e282c2ea6e3301e37ccda
Renan Ferro <renan.ferro@fmd.ag>
2023-02-13 22:24:37 -0300

    feat(console): insert new method to display console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import {
   fader,
 } from './animations/route-animations';
@@ -10,14 +9,29 @@ import {
   styleUrls: ['./app.component.scss'],
   animations: [fader]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-crud';
 
   constructor(
   ) {}
 
+  ngOnInit(): void {
+
+    this.insertConsoleStylezation()
+  }
+
   getState(outlet: any) {
     // Changing the activatedRouteData.state triggers the animation
     return outlet.activatedRouteData.state;
+  }
+
+  insertConsoleStylezation(): void {
+
+    console.log('%c Renan Ferro !',
+        `font-weight: bold; 
+      padding: 2rem; 
+      font-size: 50px; 
+      color: #faca15; 
+      text-shadow: 3px 3px 0 #e64980, 6px 6px 0 #31c48d, 9px 9px 0 #ff8a4c, 12px 12px 0 #3c1ba2;`);
   }
 }
