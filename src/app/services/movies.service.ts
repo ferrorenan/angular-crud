1c6074bba860fe2465b71053be280602a9217ba1
Renan Ferro <renan.ferro@fmd.ag>
2023-02-13 21:30:33 -0300

    feat(structure): insert structure changes

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { AbstractionApiService } from './abstraction-api.service';
-import { Movies } from '../models/movies';
 import { Card } from '../models/card';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +10,34 @@ import { Card } from '../models/card';
 export class MoviesService {
 
   constructor(
-      private _abstractionApi: AbstractionApiService
+      private _abstractionApi: AbstractionApiService,
+      private _httpClient: HttpClient
   ) { }
 
   getMovies(): Observable<Card[]> {
-    return this._abstractionApi
-        .get('movies')
+    return this._httpClient
+        .get<Card[]>('marketList')
         .pipe(
             tap(response => console.log(response))
         )
   }
 
   insertMovies(movieData: any): Observable<any> {
-    return this._abstractionApi
-        .post('movies', movieData)
+    return this._httpClient
+        .post('marketList', movieData)
+  }
+
+  editProductData(productData: Card): Observable<any> {
+    return this._httpClient
+        .put<any>(`marketList/${productData.id}`, {
+          id: productData.id,
+          name: productData.name,
+          quantity: productData.quantity,
+        })
+  }
+
+  removeItemFromList(productId: string): Observable<any> {
+    return this._httpClient
+        .delete<any>(`marketList/${productId}`)
   }
 }
