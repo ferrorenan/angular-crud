f7f77081748f3d471bdc2007b6e554f60844963f
Renan Ferro <renan.ferro@fmd.ag>
2023-02-22 21:21:20 -0300

    feat(structure): update structure to delete item with ngxs

--- src/app/components/offcanvas/offcanvas.component.ts
+++ src/app/components/offcanvas/offcanvas.component.ts
@@ -1,9 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AlertService } from '../../services/alert.service';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
 import { Card } from '../../models/card';
 import { MarketService } from '../../services/market.service';
+import { Store } from '@ngxs/store';
+import { MarketListActions } from '../../store/actions/market-list.actions';
 
 @Component({
   selector: 'app-offcanvas',
@@ -20,12 +22,13 @@ export class OffcanvasComponent implements OnInit {
   constructor(
       private _marketService: MarketService,
       private _alertService: AlertService,
-      private _route: ActivatedRoute
+      private _route: ActivatedRoute,
+      private _store: Store
   ) {}
 
   ngOnInit(): void {}
 
-  submitFormValue(): void {
+  validatePasswordToDeleteItemFromMarketList(): void {
 
     Swal.fire({
       title: 'Insert the password to Remove',
@@ -38,36 +41,19 @@ export class OffcanvasComponent implements OnInit {
       showLoaderOnConfirm: true,
       preConfirm: (login) => {
         if (login != this.passWord) {
-
           this._alertService
               .showErrorFeedbackClient();
-
         } else {
-
-          this.insertNewProductToMarketList();
+          this.deleteItemFromMarketList();
         }
       },
       allowOutsideClick: () => !Swal.isLoading()
     })
   }
 
-  insertNewProductToMarketList(): void {
+  deleteItemFromMarketList(): void {
 
-    this._marketService.removeItemFromList(this.productData.id)
-        .subscribe({
-          error: ((error) => {
-            this._alertService
-                .showErrorFeedbackClient();
-          }),
-          complete: (() => {
-            this._alertService
-                .showFeedbackClient(
-                    'Yeah, done!',
-                    `The ${this.productData.name} was deleted from your market list!`,
-                    'success',
-                );
-          })
-        })
+    this._store.dispatch(new MarketListActions.DeleteItemFromMarketList(this.productData.id));
   }
 
   closeOffcanvas(): void {
@@ -83,5 +69,7 @@ export class OffcanvasComponent implements OnInit {
           ? cardsProduct.classList.add('card-deleted-animation')
           : null
     });
+
+    this._store.dispatch(new MarketListActions.GetMarketList);
   }
 }
